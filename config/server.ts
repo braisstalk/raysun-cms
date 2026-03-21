18bf35088e7e602af3a48971750619bf093df4f3
braiss <braiss@braissdeMacBook-Pro.local>
2026-03-21 15:41:21 +0800

    config: PostgreSQL database support for Railway deployment

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,8 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
   },
+  url: env('PUBLIC_URL', ''),
 });
-
-export default config;
